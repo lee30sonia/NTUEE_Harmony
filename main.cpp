151f79d65d96d53c731dd4c7b9b363715e32b52a
ChouYun <chowduen@gmail.com>
2017-05-16 13:48:21 +0800

    main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,24 @@
 #include "Greedy.cpp"
 
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
+   if(argc != 3) {
+      cout << "usage:net_open_finder <input.txt> <output.txt>" << endl;
+      return 0;
+   }
+   CommonNs::TmUsage tmusg;
+   CommonNs::TmStat stat;
+
+   CircuitMgr ccMgr;
+   tmusg.periodStart();
+   ccMgr.readCircuit(argv[1]);
+   ccMgr.greedy();
+
+   tmusg.getPeriodUsage(stat);
+   cout << "Total CPU time: " << (stat.uTime + stat.sTime) / 1000.0 << "ms" << endl;
+   cout << "memory: " << stat.vmPeak / 1024.0 << "MB" << endl;
+   ccMgr.writeOutput(argv[2]);
+
+   return 0;
 }
