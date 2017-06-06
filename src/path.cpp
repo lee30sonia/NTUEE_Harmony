17c3ea90609a6748fe91b7dfcc06c938d9971210
ChouYun <chowduen@gmail.com>
2017-06-06 16:04:47 +0800

    .

--- src/path.cpp
+++ src/path.cpp
@@ -5,10 +5,13 @@
 bool shortestPath(Point s, Point t, int layer) {
 
    // initiate the map
-   if(!_map[layer])  _map[layer] = new int[_LL.disX(_UR)][_LL.disY(_UR)];
+   if(!_levelMap[layer])  _map[layer] = new int[_LL.disX(_UR)][_LL.disY(_UR)];
+   if(!_dirMap[layer])  _dirMap[layer] = new char[_LL.disX(_UR)][_LL.disY(_UR)];
    for(int i=0; i<_LL.disX(_UR)]; i++)
-      for(int j=0; j<_LL.disY(_UR)]; j++)
-         _map[layer][i][j] = -1;
+      for(int j=0; j<_LL.disY(_UR)]; j++) {
+         _levelMap[layer][i][j] = -1;
+         _dirMap[layer][i][j] = 0;
+      }
 
    Obstacle* it;
    for(unsigned n=0; n<_obstacles[layer].size(); n++) {
@@ -21,39 +24,44 @@ bool shortestPath(Point s, Point t, int layer) {
    int current = -1, dis2t, level;
    bool reach = false;
    Point p;
-   vector<queue<Point>> dist;
-   dist.push_back(queue<Point>());
+   if(_Q)   delete _Q;
+   _Q = new vector<queue<Point>>;
+   _Q.push_back(queue<Point>());
    s.level(layer) = 0;
-   dist[0].push(s);
+   _Q[0].push(s);
 
+   // update the levels
    while(!reach) {
       current++;
-      dist.push_back(queue<Point>());
-      while(!dist(current).empty()) {
-         p = dist(current).front();
-         dist(current).pop();
-         dis2t = p.distXY(t);
+      _Q.push_back(queue<Point>());
+      while(!_Q[current].empty()) {
+         p = _Q[current].front();
+         _Q[current].pop();
+         dis2t = p.disXY(t);
          level = p.level(layer);
-         reach = check4short(Point(p.x()-1,p.y()), t, layer, dis2t, level, dist);
-         reach = check4short(Point(p.x()+1,p.y()), t, layer, dis2t, level, dist);
-         reach = check4short(Point(p.x(),p.y()-1), t, layer, dis2t, level, dist);
-         reach = check4short(Point(p.x(),p.y()+1), t, layer, dis2t, level, dist);
+         
+         reach = check4short(Point(p.x()-1,p.y()), t, layer, 'r', dis2t, level);
+         reach = check4short(Point(p.x()+1,p.y()), t, layer, 'l', dis2t, level);
+         reach = check4short(Point(p.x(),p.y()-1), t, layer, 'u', dis2t, level);
+         reach = check4short(Point(p.x(),p.y()+1), t, layer, 'd', dis2t, level);
       }
    }
    
+   // collect the path
+
 
 }
 
-bool check4short(Point p, const Point& t, const int& layer
-      const int& dis2t, const int& level, const vector<queue<Point>>& dist) {
+bool check4short(Point p, const Point& t, const int& layer, const char& dir,
+      const int& dis2t, const int& level) {
    if(p.x()<0 || p.x()>(_LL.disX(_UR))) return false; 
    if(p.y()<0 || p.y()>(_LL.disY(_UR))) return false; 
    if(p.level(layer) >= 0)   return false;
-   if(p.distXY(t) == 0) return true;
+   if(p.disXY(t) == 0) return true;
    int level2;
-   if(p.distXY(t) > dis2t) level2 = level+1;
+   if(p.disXY(t) > dis2t) level2 = level+1;
    else  level2 = level;
    p.level(layer) = level2;
-   dist[level2].push(p);
+   _Q[level2].push(p);
    return false;
 }
